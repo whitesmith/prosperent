07aa551c68cb59141d2950ebae8052f53c11e4b8
João Barbosa <j.barbosa333@gmail.com>
2014-06-03 18:35:37 +0100

    Added products, brands and celebrities endpoints

--- lib/prosperent/client.rb
+++ lib/prosperent/client.rb
@@ -42,13 +42,41 @@ module Prosperent
 
       # Merge the config values from the module and those passed
       # to the client.
-      merged_options = Prosperent.options.merge(options)
+      @merged_options = Prosperent.options.merge(options)
  
       # Copy the merged values to this client and ignore those
       # not part of our configuration
       Configuration::VALID_CONFIG_KEYS.each do |key|
-        send("#{key}=", merged_options[key])
+        send("#{key}=", @merged_options[key])
       end
+    end
+
+    def get_products params, options={}
+      response = make_get_request("/search", params.merge(authentication_params))
+    end
+
+    def get_brands params, options={}
+      response = make_get_request("/brand", params.merge(authentication_params))
+    end
+
+    def get_celebrities params, options={}
+      response = make_get_request("/celebrity", params.merge(authentication_params))
+    end
+
+    private
+
+    def make_post_request url, body, headers = {'Content-Type' => 'application/json'}
+      self.class.post(url.to_s, :body => body, :headers => headers)
+    end
+
+    def make_get_request url, params
+      self.class.get(url.to_s, query: params)
+    end
+
+    def authentication_params
+      {
+        api_key: @merged_options[:api_key]
+      }
     end
  
   end # Client
